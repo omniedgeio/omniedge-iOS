cfa8b8072f32e33e99874d011fb3d85b0421f59c
zteshadow <samuel.song.bc@gmail.com>
2021-06-07 20:51:23 +0800

    route all IP traffic to tun

--- Tunnel/PacketTunnelProvider.swift
+++ Tunnel/PacketTunnelProvider.swift
@@ -28,6 +28,7 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
                 os_log(.default, log: self.log, "Omniedge Did setup tunnel")
                 let settings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: "151.11.50.180");
                 let ipV4 = NEIPv4Settings.init(addresses: ["10.254.1.69"], subnetMasks: ["255.255.255.0"]);
+                ipV4.includedRoutes = [NEIPv4Route.default()];
                 settings.ipv4Settings = ipV4;
                 self.setTunnelNetworkSettings(settings) { error in
                     os_log(.default, log: self.log, "Did setup tunnel settings: %{public}@, error: %{public}@", "\(settings)", "\(String(describing: error))")
