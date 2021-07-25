5210f145c9e0c66dd150eec1d4e8f5689f9efa11
zteshadow <samuel.song.bc@gmail.com>
2021-07-25 20:41:49 +0800

    [TODO]have troubles sharing config between host and extension, add a default value for empty data

--- OmniedgeDylib/OmniEdgeConfig.swift
+++ OmniedgeDylib/OmniEdgeConfig.swift
@@ -35,30 +35,36 @@ public struct OmniEdgeConfig : Codable {
     }
     
     public func sync() {
-        let dataStorage = UserDefaults(suiteName: OmniEdgeConfig.group)
-        dataStorage?.setValue(networkName, forKey: OmniEdgeConfig.networkName)
-        dataStorage?.setValue(encryptionKey, forKey: OmniEdgeConfig.encryptionKey)
-        dataStorage?.setValue(ipAddress, forKey: OmniEdgeConfig.ipAddress)
-        dataStorage?.setValue(isSecure, forKey: OmniEdgeConfig.isSecure)
-        dataStorage?.synchronize()
+        if let dataStorage = UserDefaults.init(suiteName: OmniEdgeConfig.group) {
+            dataStorage.setValue(networkName, forKey: OmniEdgeConfig.networkName)
+            dataStorage.setValue(encryptionKey, forKey: OmniEdgeConfig.encryptionKey)
+            dataStorage.setValue(ipAddress, forKey: OmniEdgeConfig.ipAddress)
+            dataStorage.setValue(isSecure, forKey: OmniEdgeConfig.isSecure)
+            dataStorage.synchronize()
+        }
     }
     
     private mutating func load() {
-        let dataStorage = UserDefaults(suiteName: OmniEdgeConfig.group)
-        if let network = dataStorage?.string(forKey: OmniEdgeConfig.networkName) {
-            networkName = network
-        }
-        
-        if let key = dataStorage?.string(forKey: OmniEdgeConfig.encryptionKey) {
-            encryptionKey = key
-        }
-        
-        if let ip = dataStorage?.string(forKey: OmniEdgeConfig.ipAddress) {
-            ipAddress = ip
-        }
-        
-        if let secure = dataStorage?.bool(forKey: OmniEdgeConfig.isSecure) {
-            isSecure = secure
+        if let dataStorage = UserDefaults.init(suiteName: OmniEdgeConfig.group) {
+            if let network = dataStorage.string(forKey: OmniEdgeConfig.networkName) {
+                networkName = network
+            } else {
+                networkName = "mynetwork"
+            }
+            
+            if let key = dataStorage.string(forKey: OmniEdgeConfig.encryptionKey) {
+                encryptionKey = key
+            } else {
+                encryptionKey = "mysecretpass"
+            }
+            
+            if let ip = dataStorage.string(forKey: OmniEdgeConfig.ipAddress) {
+                ipAddress = ip
+            } else {
+                ipAddress = "10.254.1.123"
+            }
+            
+            isSecure = dataStorage.bool(forKey: OmniEdgeConfig.isSecure)
         }
     }
 }
