2639fedcdb562341d68566a0bbb8452dc2e4450b
zteshadow <samuel.song.bc@gmail.com>
2022-02-20 20:43:32 +0800

    ping

--- Libraries/DeviceList/DeviceList/DataStore/DevicePingProvider.swift
+++ Libraries/DeviceList/DeviceList/DataStore/DevicePingProvider.swift
@@ -10,12 +10,14 @@ import Foundation
 class DevicePingProvider: DevicePingAPI {
     func ping(_ ip: String, _ complete: @escaping (Double) -> Void) {
         // Ping once
-        let once = try? SwiftyPing(host: ip, configuration: PingConfiguration(interval: 0.5, with: 5), queue: DispatchQueue.global())
+        let once = try? SwiftyPing(host: ip, configuration: PingConfiguration(interval: 1.0, with: 3), queue: DispatchQueue.global())
         once?.observer = { (response) in
             var duration = response.duration * 1000.0
+            #if false
             if response.error != nil {
                 duration = -1
             }
+            #endif
             complete(duration)
         }
         once?.targetCount = 1
